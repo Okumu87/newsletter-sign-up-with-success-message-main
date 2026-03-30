71adbf138ebf2688f44d33ea68d2018728f6bb9e
okumu87 <okumu793@gmail.com>
2026-03-31 01:05:48 +0200

    finished desktop working on states

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+
+const emailInput = document.querySelector('.email');
+
+emailInput.addEventListener('input', () => {
+    const email = emailInput.value;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailPattern.test(email)) {
+        emailInput.style.borderColor = 'green';
+    } else {
+        emailInput.style.borderColor = 'red';
+    }
+});
