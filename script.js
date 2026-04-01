fcccaa4427572f20f0828759711c965b72e0c425
okumu87 <okumu793@gmail.com>
2026-04-01 20:30:56 +0200

    js logic updated

--- script.js
+++ script.js
@@ -4,25 +4,22 @@ const main = document.querySelector('main');
 const form = document.querySelector('form');
 const successMessage = document.querySelector('.success-message');
 
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const email = emailInput.value;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (emailPattern.test(email)) {
-        successMessage.querySelector('.email').textContent = email;
-        successMessage.classList.remove('hidden');
-        main.style.display = 'none';
-        form.reset();
-        emailInput.style.borderColor = '';
-        document.querySelector('.error-message').classList.add('hidden');
-       
-    } else {
-        emailInput.style.borderColor = 'red';
-        emailInput.style.backgroundColor = '#ffe6e6';
-        document.querySelector('.error-message').classList.remove('hidden');
+    const emailValue = emailInput.value;
+    if(emailValue === ''){
+        alert('Please enter your email address');
+        return;
     }
+    successMessage.querySelector('.email').textContent = emailValue;
+    main.hidden = true;
+    successMessage.classList.remove('hidden');
+})
 
-     
-});
-
+const dismissBtn = document.getElementById('dismissBtn');
+dismissBtn.addEventListener('click', () => {
+    main.hidden = false;
+    successMessage.classList.add('hidden');
+    form.reset();
+})  
