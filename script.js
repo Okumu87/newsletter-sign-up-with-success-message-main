f7adaad3ebdd3dbfc2b806f95f02a69413dc1240
okumu87 <okumu793@gmail.com>
2026-04-01 20:02:44 +0200

    working on js logic

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 
 const emailInput = document.querySelector('.email');
+const main = document.querySelector('main');
+const form = document.querySelector('form');
+const successMessage = document.querySelector('.success-message');
 
-emailInput.addEventListener('input', () => {
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
     const email = emailInput.value;
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (emailPattern.test(email)) {
-        emailInput.style.borderColor = 'green';
+        successMessage.querySelector('.email').textContent = email;
+        successMessage.classList.remove('hidden');
+        main.style.display = 'none';
+        form.reset();
+        emailInput.style.borderColor = '';
+        document.querySelector('.error-message').classList.add('hidden');
+       
     } else {
         emailInput.style.borderColor = 'red';
+        emailInput.style.backgroundColor = '#ffe6e6';
+        document.querySelector('.error-message').classList.remove('hidden');
     }
+
+     
 });
+
