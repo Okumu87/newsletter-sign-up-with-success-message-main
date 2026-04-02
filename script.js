ff6c399194cae2e0dd6f79fbcdfae2e19a511395
okumu87 <okumu793@gmail.com>
2026-04-02 11:42:19 +0200

    still working on the code

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const emailInput = document.querySelector('.email');
 const main = document.querySelector('main');
 const form = document.querySelector('form');
 const successMessage = document.querySelector('.success-message');
+const errorMessage = document.querySelector('.error-message');
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const emailValue = emailInput.value;
-    if(emailValue === ''){
-        alert('Please enter your email address');
+    if(emailValue === '' || !emailValue) {
+        errorMessage.classList.remove('hidden');
         return;
     }
     successMessage.querySelector('.email').textContent = emailValue;
